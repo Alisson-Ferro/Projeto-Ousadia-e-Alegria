45fdf918bd6c97f1bc244b9fd2f55d8a8fd6a89b
Alisson Ferro de Souza <160792825+Alisson-Ferro@users.noreply.github.com>
2024-06-03 13:49:28 -0300

    Manutenção da logica da parte do contato e graficos da dash

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,11 +1,39 @@
-
 CREATE DATABASE projetoNey;
 
 USE projetoNey;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50)
 );
+
+CREATE TABLE contato (
+  idContato INT AUTO_INCREMENT PRIMARY KEY,
+  fkUsuario INT NOT NULL,
+  mensagem TEXT,
+  data_envio DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+
+CREATE TABLE pontuacao(
+  idPontuacao INT AUTO_INCREMENT PRIMARY KEY,
+  qtdPontos INT NOT NULL
+);
+
+CREATE TABLE quiz(
+  idQuiz INT AUTO_INCREMENT,
+  fkUsuario INT,
+  fkPontuacao INT,
+  dataRealizado DATETIME,
+  PRIMARY KEY (idQuiz, fkUsuario, fkPontuacao),
+  FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+  FOREIGN KEY (fkPontuacao) REFERENCES pontuacao(idPontuacao)
+);
+
+
+select * from usuario;
+select * from contato;
+select * from quiz;
+select * from pontuacao;
--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -18,10 +18,18 @@ function buscarPorCnpj(cnpj) {
   return database.executar(instrucaoSql);
 }
 
-function cadastrar(razaoSocial, cnpj) {
-  var instrucaoSql = `INSERT INTO empresa (razao_social, cnpj) VALUES ('${razaoSocial}', '${cnpj}')`;
-
+function cadastrar(nome, email, mensagem) {
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, mensagem);
+  
+  // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+  //  e na ordem de inserção dos dados.
+  var instrucaoSql = `
+      INSERT INTO contato (nome, email, mensagem) VALUES ('${nome}', '${email}','${mensagem}');
+  `;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
+
+
 
 module.exports = { buscarPorCnpj, buscarPorId, cadastrar, listar };
