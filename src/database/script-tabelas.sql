c257dc4ebfb9899c25fc3d016df08e9968ba81da
Alisson Ferro de Souza <160792825+Alisson-Ferro@users.noreply.github.com>
2024-06-04 00:11:23 -0300

    Alterações de cores

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -13,7 +13,9 @@ CREATE TABLE contato (
   nome varchar(100),
   email varchar(100),
   mensagem TEXT,
-  data_envio DATETIME DEFAULT CURRENT_TIMESTAMP
+  fkUsuario INT,
+  data_envio DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
 );
 
 
@@ -39,36 +41,6 @@ select * from contato;
 SELECT COUNT(idQuiz) FROM quiz;
 select * from quiz;
 select * from pontuacao;
-
--- Inserir uma nova pontuação na tabela pontuacao
-INSERT INTO pontuacao (qtdPontos) 
-VALUES (10);
-
--- Obter o idPontuacao da nova pontuação
-SELECT idPontuacao FROM pontuacao ORDER BY idPontuacao DESC LIMIT 1; 
-
--- Inserir um novo quiz na tabela quiz
-INSERT INTO quiz (fkUsuario, fkPontuacao, dataRealizado) 
-VALUES (1, 1, NOW());
-  mensagem TEXT,
-  data_envio DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
-);
-
-CREATE TABLE pontuacao(
-  idPontuacao INT AUTO_INCREMENT PRIMARY KEY,
-  qtdPontos INT NOT NULL
-);
-
-CREATE TABLE quiz(
-  idQuiz INT AUTO_INCREMENT,
-  fkUsuario INT,
-  fkPontuacao INT,
-  dataRealizado DATETIME,
-  PRIMARY KEY (idQuiz, fkUsuario, fkPontuacao),
-  FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
-  FOREIGN KEY (fkPontuacao) REFERENCES pontuacao(idPontuacao)
-);
 
 
 select * from usuario;
